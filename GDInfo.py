831aa45dba0c7cd9f90d788adab6664bd6186c39
kelptaken <77654615+kelptaken@users.noreply.github.com>
2021-10-12 20:28:34 +1000

    update v1.2

--- GDInfo.py
+++ GDInfo.py
@@ -31,6 +31,7 @@ def levelSearch():
     print('Длительность: ' + levelLength)
     print('Музыка: ' + levelSongAuthor + ' - ' + levelSongName)
     print('ID музыки: ' + str(levelSongID))
+    print('Музыка на Newgrounds: https://www.newgrounds.com/audio/listen/' + str(levelSongID))
     if levelObjects == 0:
         print('Объектов: неизвестно, копирование уровня ограничено')
     else:
@@ -137,7 +138,147 @@ def accountExportJson():
     accountExportJsonFile.close()
     print('Успешно!')
 
-print('GDInfo, v. 1.11')
+def levelSearchDebug():
+    print('!!! ВКЛЮЧЕН РЕЖИМ РАЗРАБОТЧИКА !!!')
+    levelInput = input('Введите название уровня или ID: ')
+    levelJson = r.get('https://gdbrowser.com/api/search/' + levelInput + '?count=1')
+    levelInfo = json.loads(levelJson.text)
+
+    print()
+    print('Request sent to: https://gdbrowser.com/api/search/' + levelInput + '?count=1')
+    print('API response:')
+    print(levelJson.text)
+    print()
+    print('Converted to dict:')
+    print(levelInfo)
+
+    levelName = levelInfo[0]['name']
+    levelId = levelInfo[0]['id']
+    levelDesc = levelInfo[0]['description']
+    levelCreator = levelInfo[0]['author']
+    levelDifficulty = levelInfo[0]['difficulty']
+    levelDownloads = levelInfo[0]['downloads']
+    levelLikes = levelInfo[0]['likes']
+    levelLength = levelInfo[0]['length']
+    levelSongID = levelInfo[0]['customSong']
+    levelSongAuthor = levelInfo[0]['songAuthor']
+    levelSongName = levelInfo[0]['songName']
+    levelObjects = levelInfo[0]['objects']
+
+    print()
+    print('In vars:')
+    print('levelName: ' + levelName)
+    print('levelId: ' + levelId)
+    print('levelDesc: ' + levelDesc)
+    print('levelCreator: ' + levelCreator)
+    print('levelDifficulty: ' + levelDifficulty)
+    print('levelDownloads: ' + str(levelDownloads))
+    print('levelLikes: ' + str(levelLikes))
+    print('levelLength: ' + levelLength)
+    print('levelMusic: ' + levelSongAuthor + ' - ' + levelSongName)
+    print('levelSongID: ' + str(levelSongID))
+    print('levelMusicNG: https://www.newgrounds.com/audio/listen/' + str(levelSongID))
+    if levelObjects == 0:
+        print('levelObjects = 0')
+    else:
+        print(str(levelObjects) + ' objects')
+    print()
+
+def accountSearchDebug():
+    print('!!! ВКЛЮЧЕН РЕЖИМ РАЗРАБОТЧИКА !!!')
+    accountInput = input('Введите имя аккаунта или ID: ')
+    accountJson = r.get('https://gdbrowser.com/api/profile/' + accountInput)
+    accountInfo = json.loads(accountJson.text)
+    
+    accountName = accountInfo['username']
+    accountId = accountInfo['accountID']
+    accountPlayerID = accountInfo['playerID']
+    accountStars = accountInfo['stars']
+    accountDiamonds = accountInfo['diamonds']
+    accountOffCoins = accountInfo['coins']
+    accountUserCoins = accountInfo['userCoins']
+    accountDemons = accountInfo['demons']
+    accountCP = accountInfo['cp']
+    accountFriendRequests = accountInfo['friendRequests']
+    accountMessages = accountInfo['messages']
+    accountCommentHistory = accountInfo['commentHistory']
+    accountIsModerator = accountInfo['moderator']
+    accountYouTube = str(accountInfo['youtube'])
+    accountTwitter = str(accountInfo['twitter'])
+    accountTwitch = str(accountInfo['twitch'])
+    
+    print()
+    print('Request sent to https://gdbrowser.com/api/profile/' + accountInput)
+    print('API response:')
+    print(accountJson.text)
+    print()
+    print('Converted to dict:')
+    print(accountInfo)
+
+    print()
+    print('In vars:')
+    print('accountName: ' + accountName)
+    print('accountId: ' + str(accountId))
+    print('accountPlayerID: ' + str(accountPlayerID))
+    print('accountStars: ' + str(accountStars))
+    print('accountDiamonds: ' + str(accountDiamonds))
+    print('accountOffCoins: ' + str(accountOffCoins))
+    print('accountUserCoins: ' + str(accountUserCoins))
+    print('accountDemons: ' + str(accountDemons))
+    print('accountCP: ' + str(accountCP))
+    if accountIsModerator == 0:
+        print('accountIsModerator = 0')
+    elif accountIsModerator == 1:
+        print('accountIsModerator = 1')
+    elif accountIsModerator == 2:
+        print('accountIsModerator = 2')
+    else:
+        print('wrong_response')
+    
+    print()
+    
+    print('security: ')
+    if accountFriendRequests == True:
+        print('accountFriendRequests = True')
+    else:
+        print('accountFriendRequests = False')
+    
+    if accountMessages == 'all':
+        print('accountMessages = all')
+    elif accountMessages == 'friends':
+        print('accountMessages = friends')
+    elif accountMessages == 'off':
+        print('accountMessages = off')
+    else:
+        print('wrong_response')
+    
+    if accountCommentHistory == 'all':
+        print('accountCommentHistory = all')
+    elif accountCommentHistory == 'friends':
+        print('accountCommentHistory = friends')
+    elif accountCommentHistory == 'off':
+        print('accountCommentHistory = off')
+    else:
+        print('wrong_response')
+        
+    print()
+    
+    print('social:')
+    if accountYouTube != 'None':
+        print('accountYouTube: ' + accountYouTube)
+    else:
+        print('accountYouTube: None')
+    if accountTwitter != 'None':
+        print('accountTwitter: ' + accountTwitter)
+    else:
+        print('accountTwitter: None')
+    if accountTwitch != 'None':
+        print('accountTwitch: ' + accountTwitch)
+    else:
+        print('accountTwitch: None')
+    print()
+
+print('GDInfo, v1.2')
 print('Доступные опции:')
 print('1. вывести информацию об уровне')
 print('2. вывести информацию о пользователе')
@@ -154,5 +295,9 @@ while aboba == 'bebra':
         levelExportJson()
     elif choiceInput == '4':
         accountExportJson()
+    elif choiceInput == '5':
+        levelSearchDebug()
+    elif choiceInput == '6':
+        accountSearchDebug()
     else:
         print('Неверный ответ!')
